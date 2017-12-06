1b6a0645f36928bd6c2864d4d035f1e3fa303088
tquinn35 <tquinn2035@gmail.com>
2017-12-06 11:06:43 -0700

    base

---
+++ es/views.py
@@ -0,0 +1,8 @@
+
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+def home(request):
+    return render(request, 'home.html', {})
